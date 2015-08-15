aa4a5132d4897cd4e415e26e5e77e80072480eff
Dougie3318 <dougie2593@live.co.uk>
2015-08-15 22:04:18 +0100

    Final Commit for this tutorial as it seems to be not working

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ var game = new Phaser.Game(400,490, Phaser.AUTO, 'gameDiv');
 //This is the main state and it will contain the game.
 var mainState = {
 
-    preload: function(){
+    preload: function() {
 
         //Change the background colour
        game.stage.backgroundColor = '#71c5cf';
@@ -20,14 +20,14 @@ var mainState = {
 
     create: function(){
 
-        game.physics.startSystem(phaser.Physics.Arcade);
+        game.physics.startSystem(Phaser.Physics.Arcade);
 
         this.bird = this.game.add.sprite(100, 245, 'bird');
 
         game.physics.arcade.enable(this.bird);
         this.bird.body.gravity.y = 1000;
 
-        var spacekey = this.game.input.keyboard.addKey(phaser.Keyboard.SPACEBAR);
+        var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         spaceKey.onDown.add(this.jump, this);
     },
 
@@ -37,6 +37,16 @@ var mainState = {
            this.restartGame();
 
     },
+
+    jump: function(){
+        this.bird.body.velocity.y = -350;
+    },
+
+    restartGame: function(){
+        game.state.start('main');
+
+    }
+
 };
 
 //This will add and start the main state and will then start the game
