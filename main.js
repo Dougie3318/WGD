1d58e164fba319d70d4e284c88c5ce92b1c40a4b
Dougie3318 <dougie2593@live.co.uk>
2015-08-15 20:36:51 +0100

    Added preload, create, and update functions to js file. Also included game state to start the game. Also added comments to code

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
 /**
  * Created by Douglas on 15/08/2015.
  */
+
+//This will initiate Phaser and creates a game with the specified dimensions
+var game = new Phaser.Game(400,490, Phaser.AUTO, 'gameDiv');
+
+//This is the main state and it will contain the game.
+var mainState = {
+
+    preload: function(){
+
+    },
+
+    create: function(){
+
+    },
+
+    update: function(){
+
+    },
+};
+
+//This will add and start the main state and will then start the game
+game.state.add('main', mainState);
+game.state.start('main');
